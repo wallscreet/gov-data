9f94518a9953b0b070ac97dd7b911596ddc3a648
wallscreet <wallscreet@gmail.com>
2025-08-17 16:46:08 -0500

    datasets, utils

---
+++ datasets.py
@@ -0,0 +1,252 @@
+import pandas as pd
+pd.set_option('display.max_columns', None)
+from fredapi import Fred
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+FRED_API_KEY = os.getenv("FRED_API_KEY")
+
+fred = Fred(api_key=FRED_API_KEY)
+
+
+def get_population():
+    """
+    FRED series: Population (POPTHM)
+    """
+    series = fred.get_series('POPTHM')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', 'US Population']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df = round(df.resample('YE').max(), 2)
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', 'US Population']
+    df['US Population']= df['US Population'] * 1000
+
+    return df
+
+
+def get_cpi():
+    """
+    Consumer Price Index for All Urban Consumers: All Items in U.S. City Average (CPIAUCSL)
+
+    """
+    series = fred.get_series('CPIAUCSL')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', 'CPI']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df = round(df.resample('YE').max(), 2)
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', 'CPI']
+
+    return df
+
+
+def get_pce():
+    """
+    Personal Consumption Expenditures (PCE)
+
+    """
+    series = fred.get_series('PCE')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', 'PCE']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df = round(df.resample('YE').max(), 2)
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', 'PCE']
+    df['PCE'] = df['PCE'] * 1000000000
+
+    return df
+
+
+def get_houshold_ops_spend():
+    """
+    Expenditures: Household Operations: All Consumer Units (CXUHHOPERLB0101M)
+    """
+    series = fred.get_series('CXUHHOPERLB0101M')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', 'Household Ops Annual']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', 'Household Ops Annual']
+
+    return df
+
+
+def get_vehicle_ins():
+    """
+    Expenditures: Vehicle Insurance: All Consumer Units (CXU500110LB0101M)
+    """
+    series = fred.get_series('CXU500110lB0101M')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', 'Vehicle Ins Annual']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', 'Vehicle Ins Annual']
+
+    return df
+
+
+def get_total_households():
+    """
+    Total Households (TTLH)
+    """
+    series = fred.get_series('TTLH')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', 'US Households']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', 'US Households']
+    df['US Households'] = (df['US Households'] * 1000)
+    
+    return df
+
+
+def get_pce_healthcare():
+    """
+    PCE Services: Healthcare (DHLCRC1Q027SBEA)
+    """
+    series = fred.get_series('DHLCRC1Q027SBEA')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', 'PCE Healthcare']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df = round(df.resample('YE').mean(), 2)
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', 'PCE Healthcare']
+    df['PCE Healthcare'] = df['PCE Healthcare'] * 1000000000
+
+    return df
+
+
+def get_cpi_prices_used_cars():
+    """
+    CPI Used Cars and Trucks (CUSR0000SETA02)
+
+    Prices calculated based on CPI index applied to reference year and price
+    """
+    ref_year = 2024
+    ref_price = 28472
+
+    series = fred.get_series('CUSR0000SETA02')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', 'Used Auto CPI']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df = df.resample('YE').mean()
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', 'Used Auto CPI']
+
+    ref_cpi = df.loc[df['Year'] == ref_year, 'Used Auto CPI'].values[0]
+    df['Est Avg Used Car Price'] = round((df['Used Auto CPI'] * (ref_price / ref_cpi)),2)
+
+    return df
+
+
+def get_cpi_prices_new_cars():
+    """
+    CPI New Cars and Trucks (CUUR0000SETA01)
+
+    Prices calculated based on CPI index applied to reference year and price
+    """
+    ref_year = 2024
+    ref_price = 48397
+
+    series = fred.get_series('CUUR0000SETA01')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', 'New Auto CPI']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df = df.resample('YE').mean()
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', 'New Auto CPI']
+
+    ref_cpi = df.loc[df['Year'] == ref_year, 'New Auto CPI'].values[0]
+    df['Est Avg New Car Price'] = round((df['New Auto CPI'] * (ref_price / ref_cpi)), 2)
+
+    return df
+
+
+def get_real_disposable_personal_income():
+    """
+    Real Disposable Personal Income (DSPI)
+    """
+    series = fred.get_series('DSPI')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', 'RDPI']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df = df.resample('YE').max()
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', 'RDPI']
+    df['RDPI'] = df['RDPI'] * 1000000000
+
+    return df
+
+
+def get_median_home_prices():
+    """
+    Median Sales Price of Houses Sold for the United States (MSPUS)
+    """
+    series = fred.get_series('MSPUS')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', 'Median Home Sales Price']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df = df.resample('YE').mean()
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', 'Median Home Sales Price']
+
+    return df
+
+
+def get_median_family_income():
+    """
+    Median Annual Family Income in the United States (MEFAINUSA646N)
+    """
+    series = fred.get_series('MEFAINUSA646N')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', 'Median Family Income']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', 'Median Family Income']
+
+    return df
+
+
+def get_30yr_mortgage_rates():
+    """
+    30-Year Fixed Rate Mortgage Average in the United States (MORTGAGE30US)
+    """
+    series = fred.get_series('MORTGAGE30US')
+    df = series.to_frame().reset_index()
+    df.columns = ['Date', '30yr Mortgage Rate']
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df = df.resample('YE').mean()
+    df.index = df.index.year
+    df.reset_index(inplace=True)
+    df.columns = ['Year', '30yr Mortgage Rate']
+
+    return df
---
+++ utils.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from functools import reduce
+
+
+def scale_for_inflation(cpi_df: pd.DataFrame, from_year: int, to_year: int, amount: float):
+    from_year_cpi = cpi_df.loc[cpi_df['Year'] == from_year, 'CPI'].values[0]
+    to_year_cpi = cpi_df.loc[cpi_df['Year'] == to_year, 'CPI'].values[0]
+    adjusted_value = (amount * (to_year_cpi / from_year_cpi))
+    
+    return round(adjusted_value, 2)
+
+
+def merge_on_year(dfs, how='inner'):
+    """
+    Merge a list of dataframes on the 'Year' column.
+    """
+    # Safety check: make sure they all have 'Year' column
+    for i, df in enumerate(dfs):
+        if 'Year' not in df.columns:
+            raise ValueError(f"DataFrame at index {i} is missing 'Year' column.")
+
+    merged_df = reduce(lambda left, right: pd.merge(left, right, on='Year', how=how), dfs)
+    return merged_df
